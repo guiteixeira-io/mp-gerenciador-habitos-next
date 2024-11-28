b69a57a90572047a38a603da77f7649b801959da
Guilherme Teixeira <41587365+guiteixeira-io@users.noreply.github.com>
2024-11-28 20:47:00 -0300

    Creating the home page structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Dosis, Inter } from "next/font/google";
 import Image from "next/image";
 
 
-const dosis = Dosis ({ subsets: ["latin"], variable: "--font-dosis"});
-const inter = Inter ({ subsets: ["latin"], variable: "--font-inter"});
+const dosis = Dosis({ subsets: ["latin"], variable: "--font-dosis" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Meta Diaria - Gerenciador de habitos",
@@ -19,9 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dosis.variable} ${inter.variable} flex item-center flex-col mt-10 bg-neutral-900`}> 
+      <body className={`${dosis.variable} ${inter.variable} flex item-center flex-col mt-10 bg-neutral-900 text-center`}>
         <img src="/images/logo.svg" width={200} height={200} alt="logo"></img>
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
-  const habits = {};
-  return <main>
-      </main>
-  
+  const habits = {
+    
+  };
+
+  return (<div className="container relative flex flex-col gap-8 px-4 pt-16 text-center">
+    {habits === null ||
+      (Object.keys(habits).length === 0 && (
+        <h1 className="mt-20 text-4xl font-light text-white font-display text-center">
+          Voce nao tem habitos cadastrados
+        </h1>
+      ))}
+  </div>
+  );
 }
