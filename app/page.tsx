e814a53d6642f927efb8057dea0cd3ea3306ab76
Guilherme Teixeira <41587365+guiteixeira-io@users.noreply.github.com>
2024-11-28 22:31:07 -0300

    Creating the habit list

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import DayState from "@/components/DayState";
 import Image from "next/image";
 
 export default function Home() {
   const habits = {
-    
+    'beber agua': {
+      '2023-07-18': true,
+      '2023-07-17': false,
+      '2023-07-16': false,
+    },
+    'estudar programacao': {
+      '2023-07-18': true,
+      '2023-07-17': false,
+      '2023-07-16': false,
+    },
   };
+
+  const weekDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 
   return (<div className="container relative flex flex-col gap-8 px-4 pt-16 text-center">
     {habits === null ||
@@ -12,6 +24,30 @@ export default function Home() {
           Voce nao tem habitos cadastrados
         </h1>
       ))}
+    {habits != null &&
+      Object.entries(habits).map(([habit, habitStreak]) => (
+        <div key={habit} className="text-white flex flex-col - gap-2">
+          <div className="flex justify-between items-center">
+            <span className="text-xl fonte-light text-white font-sans">{habit}</span>
+            <button>
+              <img src="/images/trash.svg" width={20} height={20} alt="Icone lixeira vermelha" />
+            </button>
+          </div>
+          <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+            {weekDays.map((day) => (
+              <div key={day} className="flex flex-col">
+                <span className="font-sans text-xs text-white text-center">
+                  {day}
+                </span>
+                {/*day state*/}
+                <DayState day={false}/>
+              </div>
+            ))}
+          </section>
+        </div>
+      )
+      )
+    }
   </div>
   );
 }
---
+++ components/DayState.tsx
@@ -0,0 +1,20 @@
+function DayState({ day }: { day: boolean | undefined }) {
+    let image: [string, string, number?] = ['/images/gray-mark.svg', 'gray-mark', 12];
+
+if (day == true) image = ['/images/check.svg', 'check', 24];
+if (day == false) image = ['/images/x.svg', 'check', 24];
+
+    const [src, alt, size] = image;
+
+    return (
+        <div className="flex items-center justify-center h-9">
+             <img 
+             src={src} 
+             width={size} 
+             eight={size} 
+             alt={alt} />
+             </div>
+    );
+}
+
+export default DayState;
